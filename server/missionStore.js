f1ffad7ff2cfb349f8d513ec265c3e1c145f2aeb
Dizzy B High <dizzybhigh@gmail.com>
2026-07-26 17:19:17 +0200

    Hide non-scan cards; add gathering keyword; mission spread count; overlay drag by default

--- server/missionStore.js
+++ server/missionStore.js
@@ -12,7 +12,7 @@ const COMMON_RESOURCES = [
 /** Battaglia ore-scan style missions only (title keywords) */
 function isMiningScanTitle(title) {
   const s = String(title || "").toLowerCase();
-  return /mining|scan|ore/.test(s);
+  return /mining|scan|ore|gathering/.test(s);
 }
 
 class Mission {
